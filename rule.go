42291cae7e22a5d97034749190b962221d28d908
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-10-22 13:55:13 +0600

    rule: fix regexp

--- rule.go
+++ rule.go
@@ -39,7 +39,7 @@ func (rules *Rules) Compile() error {
 		for _, mask := range rule.Masks {
 			glob := strings.Replace(mask, "*", "_GROD_", -1)
 			pattern := strings.Replace(
-				regexp.QuoteMeta(glob), "_GROD_", "([^/])", -1,
+				regexp.QuoteMeta(glob), "_GROD_", "([^/]+)", -1,
 			)
 
 			re, err := regexp.Compile(pattern)
@@ -63,8 +63,8 @@ func (rules *Rules) GetRule(modification string) (*Rule, error) {
 		}
 
 		for _, re := range rule.regexps {
-			matches := re.FindAllStringSubmatch(modification, -1)
-			if len(matches) > 0 {
+			match := re.MatchString(modification)
+			if match {
 				return rule, nil
 			}
 		}
