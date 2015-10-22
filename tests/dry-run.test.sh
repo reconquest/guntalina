20df90f0c63dd5e166f7c97744807c323ba93674
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-10-22 12:50:54 +0600

    add tests for dry-run mode

---
+++ tests/dry-run.test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+init
+
+config <<DATA
+actions:
+    reload-something:
+        commands:
+            - echo command_1
+            - echo command_2
+    should-be-not-executed:
+        commands:
+            - echo something went wrong
+
+rules:
+    - masks:
+        - /etc/something/something.conf
+        - /etc/something/conf.d/*
+      workflow:
+        - reload-something
+    - masks:
+        - /should/be/not/globbed/
+        - /should/*/*/globbed/
+      workflow:
+        - should-be-not-executed
+DATA
+
+sources <<DATA
+/etc/hosts
+/etc/host.conf
+/etc/something/something.conf
+/should/blah/
+/blah/globbed/
+DATA
+
+
+tests_ensure run --dry-run
+
+tests_ensure assert_diff <<DATA
+following actions will be executed:
+reload-something
+    echo command_1
+    echo command_2
+
+following commands will be executed:
+echo command_1
+echo command_2
+
+DATA
